e8a2c4b6579cba0c6be08a5a98ffe5d036273ef6
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2023-12-03 15:19:02 +0530

    made splash screen for new ui

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -2,18 +2,23 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:malpani/screen/homepage/view/HomePage.dart';
 import 'package:malpani/screen/landingpage/view/LandingPage.dart';
 import 'package:malpani/screen/more/view/More.dart';
+import 'package:malpani/screen/splashscreen/view/SplashScreen.dart';
 
-String initialPage = "/landing";
+String initialPage = "/splash";
 
 List<GetPage> pages = [
+  // GetPage(
+  //   name: '/home',
+  //   page: () => const HomePage(),
+  // ),
+  // GetPage(
+  //   name: '/landing',
+  //   page: () => const LandingPage(),
+  // ),
   GetPage(
-    name: '/home',
-    page: () => const HomePage(),
-  ),
-  GetPage(
-    name: '/landing',
-    page: () => const LandingPage(),
-  ),
+    name: '/splash',
+    page: () => const SplashScreen(),
+  )
   // GetPage(
   //   name: '/more',
   //   page: () => const More(),
---
+++ lib/screen/splashscreen/view/SplashScreen.dart
@@ -0,0 +1,79 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:malpani/screen/mainpage/view/MainPage.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 5), () {
+      Get.off(() => MainPage());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/Event Essence.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedTextKit(
+                animatedTexts: [
+                  TyperAnimatedText(
+                    'Event Essence',
+                    textStyle: const TextStyle(
+                      fontSize: 60.0,
+                      fontFamily: 'Aprilia',
+                      color: Color(0xFF3F5888),
+                    ),
+                    speed: const Duration(milliseconds: 100),
+                  ),
+                ],
+                totalRepeatCount: 1,
+                pause: const Duration(milliseconds: 1000),
+                displayFullTextOnTap: true,
+                stopPauseOnTap: true,
+              ),
+              AnimatedTextKit(
+                animatedTexts: [
+                  TyperAnimatedText(
+                    'your event our expertise',
+                    textStyle: const TextStyle(
+                        fontFamily: 'CuteFont',
+                        fontSize: 30.0,
+                        color: Color(0xFF004AAD)),
+                    speed: const Duration(milliseconds: 100),
+                  ),
+                ],
+                totalRepeatCount: 1,
+                pause: const Duration(milliseconds: 1000),
+                displayFullTextOnTap: true,
+                stopPauseOnTap: true,
+              ),
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
